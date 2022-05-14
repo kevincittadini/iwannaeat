8d92f09d410c897dd06f77aec246d7643bd78869
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-05-14 17:46:16 +0200

    add orm-pack for doctrine bundle and config.

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Broadway\Bundle\BroadwayBundle\BroadwayBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
