0bfabd53f7bc80b8e0a0dce4b09f660395cff4e3
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-05-17 12:09:33 +0200

    prepare test for OrderHandler when an order is placed.

--- app/src/Application/Order/OrderHandler.php
+++ app/src/Application/Order/OrderHandler.php
@@ -2,12 +2,10 @@
 
 declare(strict_types=1);
 
-
 namespace IWannaEat\Application\Order;
 
 use Broadway\CommandHandling\SimpleCommandHandler;
 
 final class OrderHandler extends SimpleCommandHandler
 {
-
 }
--- app/tests/Application/Order/OrderHandlerTest.php
+++ app/tests/Application/Order/OrderHandlerTest.php
@@ -10,17 +10,41 @@ use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
 use Broadway\EventHandling\EventBus;
 use Broadway\EventStore\EventStore;
 use IWannaEat\Application\Order\OrderHandler;
+use IWannaEat\Domain\Id;
+use IWannaEat\Domain\Order\OrderPlaced;
+use IWannaEat\Domain\Order\PlaceOrder;
 
 class OrderHandlerTest extends CommandHandlerScenarioTestCase
 {
+    private Id $orderId;
+    private \DateTimeImmutable $placedAt;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->orderId = new Id('00000000-0000-0000-0000-000000000001');
+        $this->placedAt = new \DateTimeImmutable('2022-05-10T20:10:00');
+    }
+
+    /** @test */
     public function it_places_order(): void
     {
-
+        $this->scenario
+            ->when(new PlaceOrder(
+                $this->orderId,
+                $this->placedAt
+            ))
+            ->then([
+                new OrderPlaced(
+                    $this->orderId,
+                    $this->placedAt
+                )
+            ]);
     }
 
     protected function createCommandHandler(EventStore $eventStore, EventBus $eventBus): CommandHandler
     {
-        return new OrderHandler(
-        );
+        return new OrderHandler();
     }
 }
