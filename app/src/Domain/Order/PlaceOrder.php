bbb1592f9e756fa096a685e32d1375f6a6c202a3
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-05-19 15:44:57 +0200

    update place order command with customer and products data.

--- app/src/Domain/Order/PlaceOrder.php
+++ app/src/Domain/Order/PlaceOrder.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace IWannaEat\Domain\Order;
 
+use IWannaEat\Domain\Customer\Customer;
 use IWannaEat\Domain\Id;
+use IWannaEat\Domain\Product\ProductList;
 
 final class PlaceOrder
 {
     public function __construct(
         public readonly Id $orderId,
+        public readonly Customer $customer,
+        public readonly ProductList $productList,
         public readonly \DateTimeImmutable $placedAt
     ) {
     }
--- app/tests/Domain/Order/PlaceOrderTest.php
+++ app/tests/Domain/Order/PlaceOrderTest.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace IWannaEat\Tests\Domain\Order;
 
+use IWannaEat\Domain\Customer\Customer;
+use IWannaEat\Domain\Customer\CustomerName;
+use IWannaEat\Domain\EmailAddress;
 use IWannaEat\Domain\Id;
 use IWannaEat\Domain\Order\PlaceOrder;
+use IWannaEat\Domain\Product\Product;
+use IWannaEat\Domain\Product\ProductList;
+use Money\Money;
 use PHPUnit\Framework\TestCase;
 
 class PlaceOrderTest extends TestCase
@@ -15,13 +21,39 @@ class PlaceOrderTest extends TestCase
     {
         $orderId = new Id('00000000-0000-0000-0000-000000000001');
         $placedAt = new \DateTimeImmutable();
+        $customer = new Customer(
+            new Id('00000000-0000-0000-0000-000000000011'),
+            new CustomerName('Rossi', 'Mario'),
+            new EmailAddress('test@example.com'),
+        );
+        $productList = new ProductList([
+            new Product(
+                new Id('00000000-0000-0000-0000-000000000002'),
+                'P1',
+                Money::EUR(123)
+            ),
+            new Product(
+                new Id('00000000-0000-0000-0000-000000000003'),
+                'P1',
+                Money::EUR(123)
+            ),
+            new Product(
+                new Id('00000000-0000-0000-0000-000000000004'),
+                'P1',
+                Money::EUR(123)
+            ),
+        ]);
 
         $placeOrder = new PlaceOrder(
             $orderId,
+            $customer,
+            $productList,
             $placedAt
         );
 
         $this->assertEquals($orderId, $placeOrder->orderId);
+        $this->assertEquals($customer, $placeOrder->customer);
+        $this->assertEquals($productList, $placeOrder->productList);
         $this->assertEquals($placedAt, $placeOrder->placedAt);
     }
 }
