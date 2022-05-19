7e72043890243362b8197dadf69bbbc1fcd2b529
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-05-19 09:04:30 +0200

    create interface for non-persisted entities.

---
+++ app/src/Domain/SimpleEntity.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace IWannaEat\Domain;
+
+use Broadway\ReadModel\Identifiable;
+use Broadway\Serializer\Serializable;
+
+interface SimpleEntity extends Serializable, Identifiable
+{
+}
